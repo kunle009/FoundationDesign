45e1d737e888d6078b97cfe0f33c6f596a39e050
CodedKunz <kunleyusuf858@gmail.com>
2022-07-04 22:20:56 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="FoundationDesign",
+    version="0.0.1",
+    author="Kunle Yusuf",
+    author_email="kunleyusuf858@gmail.com",
+    description=" A python module for structural analysis and design of different foundation type in accordance to the Eurocodes",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
