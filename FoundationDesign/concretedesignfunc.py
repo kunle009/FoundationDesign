be8614f8ea7a2cc1172014fdfc6172f4e6c98a87
Kunle Yusuf <89423261+kunle009@users.noreply.github.com>
2023-12-29 18:43:06 +0100

    Update concretedesignfunc.py
    Replaced the column Punching shear coefficient function to now use numpy instead of scipy

--- FoundationDesign/concretedesignfunc.py
+++ FoundationDesign/concretedesignfunc.py
@@ -3,9 +3,9 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 
-def shear_check_column_face(column_perimeter:float,fck:float,d:float):
-    '''
-    Function to check the shear at column face. This would be used to ascertain 
+def shear_check_column_face(column_perimeter: float, fck: float, d: float):
+    """
+    Function to check the shear at column face. This would be used to ascertain
     the suitability of the initial depth assumed.
 
     Parameters
@@ -23,12 +23,13 @@ def shear_check_column_face(column_perimeter:float,fck:float,d:float):
     -------
     float
         Maximum shear resistance in N/mm2
-    '''
-    Vrd_max = (0.5*column_perimeter*d*(0.6*(1-fck/250))*fck/1.5)/1000
+    """
+    Vrd_max = (0.5 * column_perimeter * d * (0.6 * (1 - fck / 250)) * fck / 1.5) / 1000
     return Vrd_max
 
-def punching_shear_column_face(d,fck,column_perimeter):
-    '''
+
+def punching_shear_column_face(d, fck, column_perimeter):
+    """
     Function to check the shear at column face.
 
     Parameters
@@ -46,14 +47,15 @@ def punching_shear_column_face(d,fck,column_perimeter):
     -------
     float
         Maximum shear resistance in N/mm2
-    '''
-    vrd_max = 0.2*(1-fck/250)*fck*column_perimeter*d
-    return round(vrd_max,3)
+    """
+    vrd_max = 0.2 * (1 - fck / 250) * fck * column_perimeter * d
+    return round(vrd_max, 3)
 
-def bending_reinforcement(m, depth, fck, fyk,length):
-    '''
+
+def bending_reinforcement(m, depth, fck, fyk, length):
+    """
     Function to calculate the area of steel required according to eurocode 2.
-    
+
     Parameters
     ----------
     m : float
@@ -66,24 +68,25 @@ def bending_reinforcement(m, depth, fck, fyk,length):
         Characteristic yield strength of reinforcement in N/mm2
     length : float
         Length of the element to be designed in mm
-    
+
     Returns
     -------
     float
         Area of steel required in mm2
-    '''
+    """
     depth = depth * 1000
-    k = (m * 10**6)/(fck*length*(depth**2.0))
+    k = (m * 10**6) / (fck * length * (depth**2.0))
     if k > 0.167:
-        print ('Compression Reinforcement required, design out of scope')
+        print("Compression Reinforcement required, design out of scope")
     la = 0.5 + math.sqrt(0.25 - (0.882 * k))
     if la >= 0.95:
         la = 0.95
-    area_of_steel = (m*10**6)/(0.87*fyk*la*depth)
-    return round(area_of_steel,0)
+    area_of_steel = (m * 10**6) / (0.87 * fyk * la * depth)
+    return round(area_of_steel, 0)
 
-def minimum_steel(fck,fyk,bt,d):
-    '''
+
+def minimum_steel(fck, fyk, bt, d):
+    """
     Function to calculate the minimum area of steel required.
 
     Parameters
@@ -101,15 +104,16 @@ def minimum_steel(fck,fyk,bt,d):
     -------
     float
         Minimum area of steel required in mm2
-    '''
+    """
     d = d * 1000
-    as_min = (0.078 * (fck ** (2/3)) / fyk) * bt * d
+    as_min = (0.078 * (fck ** (2 / 3)) / fyk) * bt * d
     limiting_as = 0.0013 * bt * d
-    minimum_as = max(as_min,limiting_as)
-    return round(minimum_as,0)
+    minimum_as = max(as_min, limiting_as)
+    return round(minimum_as, 0)
 
-def maximum_steel(length,d):
-    '''
+
+def maximum_steel(length, d):
+    """
     Function to calculate the maximum area of steel required.
 
     Parameters
@@ -123,12 +127,13 @@ def maximum_steel(length,d):
     -------
     float
         Maximum area of steel required in mm2
-    '''
+    """
     max_area = 0.04 * length * d
     return max_area
 
-def shear_stress_check_1d(d,px,fck):
-    '''
+
+def shear_stress_check_1d(d, px, fck):
+    """
     Function to determine the rectangular section in shear at a distance equivalent to 1d from the column face.
 
     Parameters
@@ -144,22 +149,28 @@ def shear_stress_check_1d(d,px,fck):
     -------
     float
         resistance of members without shear reinforcement in MPa
-    '''
-    k = min((1 + math.sqrt(200/d)),2)
-    p1 = min(px,0.02)
-    vrd_c = max((0.12*k*((100*p1*fck)**(1/3))),(0.035*(k**1.5)*(fck**0.5)))
-    return round(vrd_c,3) 
+    """
+    k = min((1 + math.sqrt(200 / d)), 2)
+    p1 = min(px, 0.02)
+    vrd_c = max(
+        (0.12 * k * ((100 * p1 * fck) ** (1 / 3))), (0.035 * (k**1.5) * (fck**0.5))
+    )
+    return round(vrd_c, 3)
+
 
 def column_punching_coefficient_k(column_ratio):
-    '''Interpolating Values of k for rectangular loaded areas from table 6.1 of the Eurocode 2.'''
-    x = [0.5,1,2,3]
-    y = np.array([(0.45,0.6,0.7,0.8)])
-    f = interp1d(x,y)
-    return float(f(column_ratio))
+    """Interpolating Values of k for rectangular loaded areas from table 6.1 of the Eurocode 2."""
+    x = np.array([0.5, 1, 2, 3])
+    y = np.array([0.45, 0.6, 0.7, 0.8])
+    if column_ratio < np.min(x) or column_ratio > np.max(x):
+        raise ValueError("column_ratio is outside the valid range for interpolation")
+    # Linear interpolation using numpy.interp
+    k = np.interp(column_ratio, x, y)
+    return k
 
 
 def reinforcement_provision(assteel, fy):
-    '''
+    """
     Reinforcement area (mm2/m) for different bar spacing.
 
     Parameters
@@ -168,7 +179,7 @@ def reinforcement_provision(assteel, fy):
         Area of steel required calculated in mm2/m
     fy : float
         Characteristic yield strength of reinforcement in N/mm2
-    
+
     Returns
     -------
     t : string
@@ -179,8 +190,8 @@ def reinforcement_provision(assteel, fy):
     sv : float
         bar spacing in mm
     asteel_provided : float
-        Area of steel provided in mm2/m 
-    '''
+        Area of steel provided in mm2/m
+    """
     if fy <= 250:
         t = "R"
     elif fy > 250 and fy <= 410:
@@ -189,7 +200,7 @@ def reinforcement_provision(assteel, fy):
         t = "T"
     elif fy > 460 and fy <= 500:
         t = "H"
-    if assteel <= 402 :
+    if assteel <= 402:
         rd = "8mm"
         dia = 8.00
     elif assteel > 402 and assteel <= 628:
@@ -213,14 +224,14 @@ def reinforcement_provision(assteel, fy):
     elif assteel > 6430:
         rd = "40mm"
         dia = 40.00
-    area = (math.pi * dia ** 2)/4
-    v1 = assteel/area
-    v = 1000/v1
-    n = int(v/25)*25
+    area = (math.pi * dia**2) / 4
+    v1 = assteel / area
+    v = 1000 / v1
+    n = int(v / 25) * 25
     if n > 200 and fy <= 250:
         n = 200
     elif n > 250 and fy > 250:
         n = 250
     sv = float(n)
-    asteel_provided = round(area * (1000/sv))
+    asteel_provided = round(area * (1000 / sv))
     return [t, rd, sv, asteel_provided]
