74fb7018927642ae723e1b94c2406313ccb3a09a
Kunle Yusuf <89423261+kunle009@users.noreply.github.com>
2024-03-12 22:33:44 +0100

    Update test_foundationdesign.py
    added a test for foundation loads method

--- tests/test_foundationdesign.py
+++ tests/test_foundationdesign.py
@@ -1,6 +1,48 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-
+import unittest
 from FoundationDesign.foundationdesign import padFoundationDesign, PadFoundation
 
+
+class PadFoundationDesignTestCase(unittest.TestCase):
+
+    def setUp(self):
+        fdn = PadFoundation(
+            foundation_length=3600,
+            foundation_width=3000,
+            column_length=450,
+            column_width=450,
+            col_pos_xdir=1800,
+            col_pos_ydir=1500,
+            soil_bearing_capacity=150,
+        )
+
+        z = fdn.foundation_loads(
+            foundation_thickness=550,
+            soil_depth_abv_foundation=0,
+            soil_unit_weight=18,
+            concrete_unit_weight=24,
+        )
+
+        fdn.column_axial_loads(permanent_axial_load=770, imposed_axial_load=330)
+        fdn.column_horizontal_loads_xdir(
+            permanent_horizontal_load_xdir=35, imposed_horizontal_load_xdir=15
+        )
+        fdn.column_moments_xdir(permanent_moment_xdir=78, imposed_moment_xdir=34)
+        self.pad_foundation = fdn
+
+    def test_foundation_loads(self):
+        pad_foundation = self.pad_foundation
+        self.assertEqual(
+            pad_foundation.foundation_loads(
+                foundation_thickness=550,
+                soil_depth_abv_foundation=0,
+                soil_unit_weight=18,
+                concrete_unit_weight=24,
+            ),
+            [13.2, 0.0],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
