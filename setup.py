0d00868afaa8400328eacafa53035351051f5122
Kunle Yusuf <89423261+kunle009@users.noreply.github.com>
2024-05-01 18:33:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-import setuptools
+from setuptools import setup, find_packages
 import os
 
 
-#store readme.md files
+# store readme.md files
 with open("README.md", "r") as fh:
     long_description = fh.read()
-#read the requirements
+# read the requirements
 with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
 
-setuptools.setup(
+setup(
     name="FoundationDesign",
+    packages=find_packages("FoundationDesign"),
     version="0.0.6",
     author="Kunle Yusuf",
     author_email="kunleyusuf858@gmail.com",
     description="A python module for structural analysis and design of different foundation type in accordance to the Eurocodes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     install_requires=requirements,
 )
