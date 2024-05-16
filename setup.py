4b31f944ad2e55c3e115fd84a7a6ffe7ac6d992d
Kunle Yusuf <89423261+kunle009@users.noreply.github.com>
2024-05-16 20:00:13 +0100

    "Updated Python version classifiers in setup.py"

--- setup.py
+++ setup.py
@@ -20,7 +20,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
